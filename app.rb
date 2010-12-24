70d1796ddeb9f6233170dc91de68bbfc062ca1e0
Micah Elliott <mde@MicahElliott.com>
2010-12-24 02:06:55 -0800

    Using more standard (at least shorter) dirs.
    static -> public
    stylesheets -> css

--- app.rb
+++ app.rb
@@ -7,17 +7,17 @@ require 'haml'    #must be loaded after sinatra
 set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
 set :views, "views"
-set :public, 'static'
+#set :public, 'static'
 set :haml, :format => :html5 # default Haml format is :xhtml
 
 configure do
   Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
 end
 
-# at a minimum, the main sass file must reside within the ./views directory. here, we create a ./views/stylesheets directory where all of the sass files can safely reside.
-get '/stylesheets/:name.css' do
+# at a minimum, the main sass file must reside within the ./views directory. here, we create a ./views/css directory where all of the sass files can safely reside.
+get '/css/:name.css' do
   content_type 'text/css', :charset => 'utf-8'
-  sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
+  sass(:"css/#{params[:name]}", Compass.sass_engine_options )
 end
 
 get '/' do
